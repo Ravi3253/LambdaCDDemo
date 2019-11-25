8d2a520c47ac08cfd45316459e4f66794887319a
Ravi3253 <35370662+Ravi3253@users.noreply.github.com>
2019-11-25 20:56:33 +0530

    Hello

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,6 +1,8 @@
-import boto3
 import json
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    print('Hello, world!!')
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
